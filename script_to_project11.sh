6c014561076caf03125f6d91c0677f7864e62f28
root <root@jenkins.ru-central1.internal>
2023-03-18 23:07:45 +0300

    Added sudo for script

--- script_to_project11.sh
+++ script_to_project11.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir /var/lib/jenkins/workspace/ci-cd-project11/
-docker run -d -p 9889:80 --name ci-cd-project11 --mount type=bind,src=/var/lib/jenkins/workspace/ci-cd-project11/,dst=/usr/share/nginx/html/ nginx
-curl -I http://localhost:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
-echo -n 'http://localhost:9889/index.html' | openssl md5 -hex
-docker stop ci-cd-project11
-docker rm ci-cd-project11
+# mkdir /var/lib/jenkins/workspace/ci-cd-project11/
+sudo docker run -d -p 9889:80 --name ci-cd-project11 --mount type=bind,src=/var/lib/jenkins/workspace/ci-cd-project11/,dst=/usr/share/nginx/html/ nginx
+sudo curl -I http://localhost:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
+sudo echo -n 'http://localhost:9889/index.html' | openssl md5 -hex
+sudo docker stop ci-cd-project11
+sudo docker rm ci-cd-project11
